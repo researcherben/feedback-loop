797b1e330d6a3d6512cf7027f2d4490756e625b5
Ben Payne <ben.is.located@gmail.com>
2023-09-03 09:25:58 -0400

    finally got multiple windows to work

--- curses-based/demo.py
+++ curses-based/demo.py
@@ -41,43 +41,91 @@ class ui:
         self.stdscr.keypad(True)
         ##### End boilerplate for curses configuration #####
 
-        # Coordinates are always passed in the order y,x,
-        # and the top-left corner of a window is coordinate (0,0).
-        # # valid coordinates extend from (0,0) to (curses.LINES - 1, curses.COLS - 1).
-        self.win1 = curses.newwin(10, 50, 0, 0)
+        # Panel functions allow the use of stacked windows and ensure the proper
+        # portions of each window and the curses stdscr window are hidden or
+        # displayed when panels are added, moved, modified or removed.
+
+        # Coordinates are always passed in the order y,x
+        # The top-left corner of a window is coordinate (0,0).
+        # Valid coordinates extend from (0,0) to (curses.LINES - 1, curses.COLS - 1).
+        active_data_window_height = 14
+        active_data_window_length = 100
+        self.win1 = curses.newwin(active_data_window_height, active_data_window_length, 0, 0)
         self.win1.border(0)
-        self.win1.addstr(1, 1, "Window 1")
+        # self.win1.addstr(1, 1, "Win 1")
         self.pan1 = curses.panel.new_panel(self.win1)
-        self.pan1.hide()
 
-        self.win2 = curses.newwin(10, 50, 0, 0)
+        self.win2 = curses.newwin(active_data_window_height, active_data_window_length, 0, 0)
         self.win2.border(0)
-        self.win2.addstr(1, 1, "Window 2")
+        #self.win2.addstr(1, 1, "           A Window 2")
         self.pan2 = curses.panel.new_panel(self.win2)
+        self.pan2.hide() # https://docs.python.org/3/library/curses.panel.html
 
-        self.win3 = curses.newwin(10, 50, 12, 0)
+        self.win3 = curses.newwin(active_data_window_height, active_data_window_length, 0, 0)
         self.win3.border(0)
-        self.win3.addstr(1, 1, "Control panel")
-        self.win3.addstr(3, 1, "Press 's' to switch windows or 'q' to quit.")
+        #self.win3.addstr(1, 1, "And now Window 3")
         self.pan3 = curses.panel.new_panel(self.win3)
+        self.pan3.hide() # https://docs.python.org/3/library/curses.panel.html
+
+        self.win_ctrl = curses.newwin(10, 50, active_data_window_height+1, 0)
+        self.win_ctrl.border(0)
+        self.win_ctrl.addstr(1, 1, "Control panel")
+        self.win_ctrl.addstr(3, 1, "Press 's' to switch windows or 'q' to quit.")
+        self.win_ctrl.addstr(8, 1, "Don't kill this using ctrl+c or ctrl-z")
+        self.pan_ctrl = curses.panel.new_panel(self.win_ctrl)
 
 
     def refresh(self):
-        curses.panel.update_panels()
-        self.win2.refresh()
+        """
+        `erase` vs `clear`: https://stackoverflow.com/a/43486979/1164295
+        """
+        # self.win1.erase()
+        # self.win1.clear()
+        # curses.panel.update_panels() # https://docs.python.org/3/library/curses.panel.html
+        # most recent (bottom of this list) is displayed
         self.win1.refresh()
+        self.win2.refresh()
+        self.win3.refresh()
+        self.win_ctrl.refresh()
+        #curses.doupdate()
 
     def switch_pan(self):
-        if self.pan1.hidden():
-            self.pan2.bottom()
-            self.pan2.hide()
-            self.pan1.top()
-            self.pan1.show()
-        else:
-            self.pan1.bottom()
-            self.pan1.hide()
+        # self.win1.addstr(5, 1, "top_panel: "+str(curses.panel.top_panel()))
+        # self.win2.addstr(5, 1, "top_panel: "+str(curses.panel.top_panel()))
+        # self.win3.addstr(5, 1, "top_panel: "+str(curses.panel.top_panel()))
+        # self.win1.addstr(6, 1, "switchin (win1)")
+        # self.win2.addstr(7, 1, "   switchin (win2)")
+        # self.win3.addstr(8, 1, "      switchin  (win3)")
+
+        # self.win1.addstr(10, 1, "pan1:"+str(self.pan1.hidden())+", pan2:"+str(self.pan2.hidden())+", pan3:"+str(self.pan3.hidden()))
+        # self.win2.addstr(11, 1, "pan1:"+str(self.pan1.hidden())+", pan2:"+str(self.pan2.hidden())+", pan3:"+str(self.pan3.hidden()))
+        # self.win3.addstr(12, 1, "pan1:"+str(self.pan1.hidden())+", pan2:"+str(self.pan2.hidden())+", pan3:"+str(self.pan3.hidden()))
+
+         # https://docs.python.org/3/library/curses.panel.html
+        if self.pan2.hidden() and self.pan3.hidden(): # 1 is shown, switch to 2
             self.pan2.top()
             self.pan2.show()
+            self.pan1.bottom()
+            self.pan1.hide()
+            self.pan3.bottom()
+            self.pan3.hide()
+        elif self.pan1.hidden() and self.pan3.hidden(): # 2 is shown, switch to 3
+            self.pan3.top()
+            self.pan3.show()
+            self.pan1.bottom()
+            self.pan1.hide()
+            self.pan2.bottom()
+            self.pan2.hide()
+        elif self.pan1.hidden() and self.pan2.hidden(): # 3 is shown, switch to 1
+            self.pan1.top()
+            self.pan1.show()
+            self.pan2.bottom()
+            self.pan2.hide()
+            self.pan3.bottom()
+            self.pan3.hide()
+        # else:
+        #     print("error")
+        #     pass
 
         self.refresh()
 
@@ -90,7 +138,7 @@ class ui:
         curses.echo()
         # call the endwin() function to restore the terminal to its original operating mode.
         curses.endwin()
-        print("UI terminated")
+        print("UI terminated\n")
         exit(0)
 
 
@@ -99,12 +147,18 @@ class feeder:
     def __init__(self):
         self.running = False
         self.ui = ui()
-        self.count = 0
+        self.count = 0 # tick
 
     def stop(self):
+        """
+        stop the update loop
+        """
         self.running = False
 
     def run(self):
+        """
+        run the update loop
+        """
         self.running = True
         self.feed()
 
@@ -119,12 +173,31 @@ class feeder:
                 elif line.strip() == "s":
                     self.ui.switch_pan()
 
-            self.ui.win1.addstr(3, 1, str(self.count)+\
-                                ": "+str(int(round(random.random()*999))))
-            self.ui.win1.addstr(4, 1, str(self.running))
-            self.ui.win2.addstr(3, 1, str(self.count)+\
-                                ": "+str(int(round(random.random()*999))))
-            self.ui.win2.addstr(4, 1, str(self.running))
+            # populate the window with random content that is dynamically changing
+            if self.ui.pan2.hidden() and self.ui.pan3.hidden(): # 1 is shown
+                self.ui.win1.erase()
+                self.ui.win1.border(0)
+                self.ui.win1.addstr(1, 1, "Window 1")
+                self.ui.win1.addstr(3, 1, str(self.count)+\
+                                 ": "+str(int(round(random.random()*99))))
+                self.ui.win1.addstr(4, 1, str(self.running))
+
+            if self.ui.pan1.hidden() and self.ui.pan3.hidden(): # 2 is shown
+                self.ui.win2.erase()
+                self.ui.win2.border(0)
+                self.ui.win2.addstr(1, 1, "Window 2")
+                self.ui.win2.addstr(3, 1, str(self.count)+\
+                                    ": "+str(int(round(random.random()*9999))))
+                self.ui.win2.addstr(4, 1, str(self.running))
+
+            if self.ui.pan1.hidden() and self.ui.pan2.hidden(): # 3 is shown
+                self.ui.win3.erase()
+                self.ui.win3.border(0)
+                self.ui.win3.addstr(1, 1, "Window 3")
+                self.ui.win3.addstr(3, 1, str(self.count)+\
+                                    ": "+str(int(round(random.random()*99999999))))
+                self.ui.win3.addstr(4, 1, str(self.running))
+
             self.ui.refresh()
             time.sleep(0.5)
             self.count += 1
